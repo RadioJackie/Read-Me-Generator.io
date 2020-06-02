9be23bf24c6a38ce3dd70d5052ba731029a71890
RadioJackie <jacobpeters628@gmail.com>
2020-06-01 21:21:09 -0700

    Did the generateMarkdown file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,6 +2,22 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description
+
+## Table of Contents
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+
 `;
 }
 
