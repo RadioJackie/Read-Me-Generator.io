5478cf91a927f4848220507b33aed6ee9273ca84
RadioJackie <jacobpeters628@gmail.com>
2020-06-01 23:20:39 -0700

    Got it to work by properly linking the generateMarkdown, I have badges now by installing npm install badge-maker, from npm. Fully works, just need to do the pics and gif

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,22 +1,38 @@
 function generateMarkdown(data) {
   return `
+
+var result = str.link(""); 
+
 # ${data.title}
 
 ## Description
 
 ## Table of Contents
 
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Testing](#testing)
+
 ## Installation
+
+${data.installation}
 
 ## Usage
 
+${data.usage}
+
 ## License
+
+${data.license}
 
 ## Contributing
 
 ## Tests
 
-## Questions
+${data.tests}
+
 
 `;
 }
